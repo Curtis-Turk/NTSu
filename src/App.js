edff5c72a9535619be8e3b9cac55b36c221bd19f
Curtis Turk <curtis.c.turk@outlook.com>
2023-01-06 14:54:45 +0000

    added textInput functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
+  const [textInput, setTextInput] = useState("");
+  const [episode, setEpisode] = useState(textInput);
+
+  const onChange = (e) => {
+    setTextInput(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    setEpisode(textInput);
+  };
+
   return (
     <div>
       <h1>NTSu</h1>
-      <input type="text"></input>
-      <h2>Paste in a NTS episode to view tracks</h2>
+      <input type="text" onChange={onChange} />
+      <button onClick={onSubmit} type="button">
+        GET TRACKS
+      </button>
+      <h2>Enter a NTS episode to view tracks</h2>
+      <p>{episode}</p>
     </div>
   );
 }
