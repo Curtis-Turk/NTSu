5305936329500995ee72a67eb86315d535cbf97a
Curtis Turk <curtis.c.turk@outlook.com>
2023-01-06 13:55:35 +0000

    styled homepage

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1 className="red">NTSu</h1>
+      <h1>NTSu</h1>
       <input type="text"></input>
+      <h2>Paste in a NTS episode to view tracks</h2>
     </div>
   );
 }
