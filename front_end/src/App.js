c67332bc264097d52ae11fd91f8c1ec4cca8db2e
Curtis Turk <curtis.c.turk@outlook.com>
2023-01-12 10:18:08 +0000

    added a connection to the backend - CORS error still occuring

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,46 +1,50 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import Header from './components/Header';
-import Scraper from './api/Scraper';
-import Tracklist from './components/Tracklist';
-import Landing from './components/Landing';
-import bandcampSearch from './api/bandcampSearch';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import Scraper from "./api/Scraper";
+import Tracklist from "./components/Tracklist";
+import Landing from "./components/Landing";
+import bandcampSearch from "./api/bandcampSearch";
 
 function App() {
-	const [episode, setEpisode] = useState('');
-	const [episodeData, setEpisodeData] = useState([]);
+  const [episode, setEpisode] = useState("");
+  const [episodeData, setEpisodeData] = useState([]);
 
-	useEffect(() => {
-		let fetchNts = async () => {
-			const ntsData = await Scraper(episode);
-			setEpisodeData(ntsData);
-		};
-		fetchNts();
-	}, [episode]);
+  useEffect(() => {
+    let fetchNts = async () => {
+      const ntsData = await Scraper(episode);
+      setEpisodeData(ntsData);
+    };
+    fetchNts();
+  }, [episode]);
 
-	useEffect(() => {
-		let apiResponse = () => {
-			console.log('logging api fetch');
-			fetch('http://localhost:3000/api')
-				.then((response) => response.json())
-				.then((data) => console.log(data));
-		};
-		apiResponse();
-	}, []);
+  useEffect(() => {
+    if (episodeData.allTracks) bandcampSearch(episodeData.allTracks[0]);
+  }, [episodeData]);
 
-	return (
-		<div>
-			<Header setEpisode={setEpisode} />
-			<Landing episode={episode} />
-			<h2>{episodeData.episodeTitle}</h2>
-			<Tracklist tracks={episodeData.allTracks} />
+  // useEffect(() => {
+  //   let apiResponse = () => {
+  //     console.log("logging api fetch");
+  //     fetch("http://localhost:3001/api")
+  //       .then((response) => response.json())
+  //       .then((data) => console.log(data));
+  //   };
+  //   apiResponse();
+  // }, []);
 
-			<div>
-				{/* {console.log('app log', episodeData.allTracks[0])} */}
-				{/* {bandcampSearch(episodeData .allTracks[0])} */}
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <Header setEpisode={setEpisode} />
+      <Landing episode={episode} />
+      <h2>{episodeData.episodeTitle}</h2>
+      <Tracklist tracks={episodeData.allTracks} />
+
+      <div>
+        {/* {console.log('app log', episodeData.allTracks[0])} */}
+        {/* {bandcampSearch(episodeData.allTracks[0])} */}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- front_end/src/api/bandcampSearch.js
+++ front_end/src/api/bandcampSearch.js
@@ -1,25 +1,48 @@
-import cheerio from 'cheerio';
+// import cheerio from "cheerio";
 
-function bandcampSearch(track) {
-	let searchTitle = track.title.split(' ').join('+');
-	let searchArtist = track.artist.split(' ').join('+');
+// const bandcampSearch = (track) => {
+//   let searchTitle = track.title.split(" ").join("+");
+//   let searchArtist = track.artist.split(" ").join("+");
 
-	const URL = `https://bandcamp.com/search?q=${searchTitle}+${searchArtist}&item_type=t&from=results`;
+//   const URL = `https://bandcamp.com/search?q=${searchTitle}+${searchArtist}&item_type=t&from=results`;
 
-	return fetch(URL)
-		.then((response) => response.text())
-		.then((html) => {
-			const $ = cheerio.load(html);
-			console.log($);
-			// $('.track').each((_, e) => {
-			// let track = { title: null, artist: null };
-			// track.title = $(e).children('.track__title').text();
-			// track.artist = $(e).children('.track__artist').first().text();
-			// allTracks.push(track);
-			// });
+//   return fetch(URL)
+//     .then((response) => response.text())
+//     .then((html) => {
+//       const $ = cheerio.load(html);
+//       console.log($);
+//       // $('.track').each((_, e) => {
+//       // let track = { title: null, artist: null };
+//       // track.title = $(e).children('.track__title').text();
+//       // track.artist = $(e).children('.track__artist').first().text();
+//       // allTracks.push(track);
+//       // });
 
-			// return { episodeTitle: episodeTitle, allTracks: allTracks };
-		});
-}
+//       // return { episodeTitle: episodeTitle, allTracks: allTracks };
+//     });
+// };
+
+const bandcampSearch = (track) => {
+  if (track) {
+    // console.log(track);
+    let searchTitle = track.title.split(" ").join("+");
+    let searchArtist = track.artist.split(" ").join("+");
+
+    const url = `https://bandcamp.com/search?q=${searchTitle}+${searchArtist}&item_type=t&from=results`;
+
+    fetch("http://localhost:3001/bandcamp", {
+      method: "POST",
+      body: JSON.stringify({ url: url }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+};
 
 export default bandcampSearch;
