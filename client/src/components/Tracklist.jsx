2529c4c235191b1afed7988c9aea3190f7715ec0
Curtis Turk <curtis.c.turk@outlook.com>
2023-01-24 20:51:17 +0000

    Added logic for if no tracklist provided

--- client/src/components/Tracklist.jsx
+++ client/src/components/Tracklist.jsx
@@ -4,7 +4,12 @@ function Tracklist({ tracks }) {
   const listTracks = tracks?.map((track, i) => {
     return <Track key={i} track={track} />;
   });
-  if (listTracks) return <ul>{listTracks}</ul>;
+
+  if (listTracks.length) {
+    return <ul>{listTracks}</ul>;
+  } else {
+    return <h3>No tracklist provided</h3>;
+  }
 }
 
 export default Tracklist;
